0cb28551278fc9b81edc8f3e91c551443c2bb2d4
IlyaVento <ilya.popov.sbmt@gmail.com>
2018-03-05 00:53:34 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+    let primeNumbers = takingPrimes(base)
+    let degr = 0
+    let middle = base
+    let maxDivider = 0
+    let count = 0
+    let zerosCount = 0
+
+    for (let i = 0; primeNumbers[i] <= base; i++) {
+        if (base % primeNumbers[i] === 0) {
+            maxDivider = primeNumbers[i]
+        }
+    }
+
+    while (middle % maxDivider === 0) {
+        degr++
+        middle /= maxDivider
+    }
+
+    while (number > 0) {
+        number = Math.floor(number / maxDivider)
+        count += number;
+    }
+
+    zerosCount = Math.floor(count / degr)
+
+    function takingPrimes(n) {
+        let arr = []
+        let primeNumbers = []
+
+        arr[1] = 0
+
+        for (let i = 2; i <= n; i++)
+            arr[i] = 1
+
+        for (let i = 2; i * i <= n; i++) {
+            if (arr[i] === 1) {
+                for (let l = i * i; l <= n; l += i) {
+                    arr[l] = 0
+                }
+            }
+        }
+
+        for (let i = 0; i <= arr.length; i++) {
+            if (arr[i] === 1)
+                primeNumbers.push(i)
+        }
+
+        return primeNumbers
+    }
+
+    return zerosCount
 }
